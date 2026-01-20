a21b57f15c9b737a4b6a495f6376bedcd794ee63
yexxjx <chenstyle02@gmail.com>
2026-01-20 16:51:22 +0900

    도경

---
+++ 점주로그인.js
@@ -0,0 +1,8 @@
+function ListLogin(){
+    const ListidDom=document.querySelector(".Listid");
+    const Listid=ListidDom.value;
+    const ListpwdDom=document.querySelector(".Listpwd");
+    const Loginpwd=ListpwdDom.value;
+
+    let loginmember
+}
