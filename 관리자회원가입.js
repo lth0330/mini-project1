bbf05e5c826f74fd0c5f0f07c6ac5cb81cdbe313
yexxjx <chenstyle02@gmail.com>
2026-01-21 16:47:49 +0900

    260121 이연지 js 수정

--- 관리자회원가입.js
+++ 관리자회원가입.js
@@ -1,39 +1,58 @@
+// 260121 이연지 전체 수정
+
+function Listcheck(){
+    const inputId=document.querySelector(".Listid").value;
+
+    let AdminInfo=localStorage.getItem("AdminInfo");
+    AdminInfo=AdminInfo?JSON.parse(AdminInfo):[];
+
+    let Listidcheck=false;
+    for(let i=0; i<AdminInfo.length; i++){
+        if(AdminInfo[i].Listid==inputId){
+            Listidcheck=true; 
+            break;}
+    }
+
+    if(Listidcheck==true){alert("사용 불가능한 아이디입니다.");} 
+    else{alert("사용 가능한 아이디입니다.");}
+}
+
 function Listreg(){
     const ListidDom=document.querySelector(".Listid");
     const Listid=ListidDom.value;
-    const ListpwdDom=document.querySelector(".Listpwd");
-    const Listpw=ListpwdDom.value;
+    const ListpwDom=document.querySelector(".Listpw");
+    const Listpw=ListpwDom.value;
+    const ListbrNumDom=document.querySelector(".ListbrNum");
+    const ListbrNum=ListbrNumDom.value;
+    const ListconvNameDom=document.querySelector(".ListconvName");
+    const ListconvName=ListconvNameDom.value;
+    const ListlocationDom=document.querySelector(".Listlocation");
+    const Listlocation=ListlocationDom.value;
+    const ListphoneDom=document.querySelector(".Listphone");
+    const Listphone=ListphoneDom.value;
 
-    let Listmember=localStorage.getItem("Listmember");
-    if(Listmember==null){Listmember=[];}
-    else{Listmember=JSON.parse(Listmember);}
+    let AdminInfo=localStorage.getItem("AdminInfo");
+    if(AdminInfo==null){AdminInfo=[];}
+    else{AdminInfo=JSON.parse(AdminInfo);}
 
-    let Listno=Listmember.length>=1? Listmember[Listmember.length-1].Listno+1:1;
-    let obj={Listno, Listid, Listpw};
-    console.log(obj);
-    Listmember.push(obj); alert("회원가입 성공");
-
-    localStorage.setItem("Listmember",JSON.stringify(Listmember));
-
-}
-
-function Listcheck(){  // 260121 이연지 수정
-    const inputId=document.querySelector(".Listid").value;
-
-    let Listmember=localStorage.getItem("Listmember");
-    Listmember=Listmember?JSON.parse(Listmember):[];
-
-    let Listidcheck=false;
-    for(let i=0; i<Listmember.length; i++) {
-        if(Listmember[i].Listid==inputId) {
-            Listidcheck=true; 
+    let Listconfirm=false;
+    for (let i=0; i<AdminInfo.length;i++){
+        if (AdminInfo[i].Listid==Listid){
+            Listconfirm = true;
             break;
         }
     }
 
-    if(Listidcheck==true){
-        alert("사용 불가능한 아이디입니다.");
-    } else{
-        alert("사용 가능한 아이디입니다.");
+    if (Listconfirm==true){
+        alert("이미 사용 중인 아이디입니다. 다른 아이디를 입력해주세요.");
+        return;
     }
+
+    let ListadminNo=AdminInfo.length>=1? AdminInfo[AdminInfo.length-1].ListadminNo+1:1;
+    let obj={ListadminNo, Listid, Listpw, ListbrNum, ListconvName, Listlocation,Listphone};
+    console.log(obj);
+    AdminInfo.push(obj); alert("회원가입 성공");
+
+    localStorage.setItem("AdminInfo",JSON.stringify(AdminInfo));
+    location.href="점주로그인.html"
 }
--- 점주로그인.js
+++ 점주로그인.js
@@ -1,20 +1,32 @@
+// 260121 이연지 전체 수정
+
 function ListLogin(){
     const ListnewidDom=document.querySelector(".Listid");
     const Listid=ListnewidDom.value;
-    const ListpwdDom=document.querySelector(".Listpwd");
-    const Listpw=ListpwdDom.value;
+    const ListpwDom=document.querySelector(".Listpw");
+    const Listpw=ListpwDom.value;
 
-    let Listmember=localStorage.getItem("Listmember");
-    if(Listmember==null){Listmember=[];}
-    else{ Listmember=JSON.parse(Listmember);}
+    let AdminInfo=localStorage.getItem("AdminInfo");
+    if(AdminInfo==null){AdminInfo=[];}
+    else{AdminInfo=JSON.parse(AdminInfo);}
+    
+    let Listsuccess=false;
 
-    for(let index=0; index<=Listmember.length-1;index++){
-        const memberlist=Listmember[index];
+    for(let index=0; index<=AdminInfo.length-1;index++){
+        const memberlist=AdminInfo[index];
         if(memberlist.Listid==Listid&&memberlist.Listpw==Listpw){
+            const AdminInfo={Listid: memberlist.Listid, Listpw: memberlist.Listpw,
+            ListbrNum: memberlist.ListbrNum, ListconvName: memberlist.ListconvName,
+            Listlocation: memberlist.Listlocation, Listphone: memberlist.Listphone}
+
+            sessionStorage.setItem("AdminInfo", JSON.stringify(AdminInfo));
+
             alert("로그인 성공");
-            location.href="productView.html" // 260121 이연지 추가
+            location.href="productView.html"
+
+            Listsuccess=true;
             return;
         }
     }
-    alert("로그인 실패");
+    if(!Listsuccess){alert("로그인 실패");}
 }
